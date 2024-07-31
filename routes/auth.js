5ecc899a04e93e90b7f0acbef695bff053633cbb
GDA0 <gideondelaliadeti0@gmail.com>
2024-07-31 19:26:53 +0000

    Rename controller files and update routes accordingly

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 
-const authController = require("../controllers/auth-controller");
+const authController = require("../controllers/auth");
 const configurePassport = require("../configure-passport");
 
 configurePassport(passport);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const indexController = require("../controllers/index-controller");
+const indexController = require("../controllers/index");
 
 router.get("/", indexController.controlIndexGet);
 
