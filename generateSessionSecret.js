7d6f61b6ff6f1257d5894a5c02273f6f736254f1
GDA0 <gideondelaliadeti0@gmail.com>
2024-08-02 10:12:45 +0000

    Rename file

--- generateSessionSecret.js
+++
@@ -1,8 +0,0 @@
-const crypto = require("crypto");
-
-const generateSessionSecret = () => {
-  return crypto.randomBytes(64).toString("hex");
-};
-
-const sessionSecret = generateSessionSecret();
-console.log("Your Express session secret is:", sessionSecret);
